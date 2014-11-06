39706d285bcc02f5cb73fe6232085756d8495a73
devboy <dominic.graefen@gmail.com>
2014-11-06 20:27:16 +0100

    Copies Contents with extra rules for Plugins/Android & Plugins/iOS

--- src/Paket.Unity3D/Constants.fs
+++ src/Paket.Unity3D/Constants.fs
@@ -2,3 +2,5 @@
 
 let [<Literal>] Unity3DReferencesFile = "paket.unity3d.references"
 let [<Literal>] Unity3DCopyFolderName = "Paket.Unity3D"
+let [<Literal>] UnityAndroidPluginPath = "Plugins/Android"
+let [<Literal>] UnityIOSPluginPath = "Plugins/iOS"
--- src/Paket.Unity3D/InstallProcess.fs
+++ src/Paket.Unity3D/InstallProcess.fs
@@ -7,11 +7,15 @@ open System.Collections.Generic
 
 let private findPackagesWithContent (usedPackages:Dictionary<_,_>) = 
     usedPackages
-    |> Seq.map (fun kv -> DirectoryInfo(Path.Combine("packages", kv.Key)))
-    |> Seq.choose (fun packageDir -> packageDir.GetDirectories("Content") |> Array.tryFind (fun _ -> true))
+    |> Seq.map (fun kv -> kv.Key, DirectoryInfo(Path.Combine("packages", kv.Key, "content")))
+    |> Seq.filter (fun (package,packageDir) -> packageDir.Exists)
     |> Seq.toList
 
-let private copyContentFiles (project : Unity3DReferencesFile, packagesWithContent) = 
+let private copyContentFiles (project : Unity3DReferencesFile, package, dir) = 
+    
+    let root:DirectoryInfo = dir
+    let androidSourceDir = DirectoryInfo(Path.Combine(root.FullName, Constants.UnityAndroidPluginPath))
+    let iOSSourceDir = DirectoryInfo(Path.Combine(root.FullName, Constants.UnityIOSPluginPath))
 
     let rules : list<(FileInfo -> bool)> = [
             fun f -> f.Name = "_._"
@@ -20,11 +24,16 @@ let private copyContentFiles (project : Unity3DReferencesFile, packagesWithConte
             fun f -> f.Name.EndsWith(".tt")
             fun f -> f.Name.EndsWith(".ttinclude")
         ]
-
+    
     let onBlackList (fi : FileInfo) = rules |> List.exists (fun rule -> rule(fi))
 
     let rec copyDirContents (fromDir : DirectoryInfo, toDir : Lazy<DirectoryInfo>) =
-        fromDir.GetDirectories() |> Array.toList
+        fromDir.GetDirectories()
+        |> Array.filter (fun d -> verbosefn "d: %A" d.FullName
+                                  verbosefn "a: %A" androidSourceDir.FullName
+                                  verbosefn "i: %A" iOSSourceDir.FullName 
+                                  not(d.FullName = androidSourceDir.FullName) && not(d.FullName = iOSSourceDir.FullName )) 
+        |> Array.toList
         |> List.collect (fun subDir -> copyDirContents(subDir, lazy toDir.Force().CreateSubdirectory(subDir.Name)))
         |> List.append
             (fromDir.GetFiles() 
@@ -32,8 +41,17 @@ let private copyContentFiles (project : Unity3DReferencesFile, packagesWithConte
                 |> List.filter (onBlackList >> not)
                 |> List.map (fun file -> file.CopyTo(Path.Combine(toDir.Force().FullName, file.Name), true)))
 
-    packagesWithContent
-    |> List.collect (fun packageDir -> copyDirContents (packageDir, lazy (DirectoryInfo(project.UnityAssetsDir))))
+    let targetDir = DirectoryInfo(Path.Combine(project.UnityAssetsDir, Constants.Unity3DCopyFolderName, package))
+    let androidTargetDir = DirectoryInfo(Path.Combine(project.UnityAssetsDir, Constants.UnityAndroidPluginPath, Constants.Unity3DCopyFolderName, package))
+    let iOSTargetDir = DirectoryInfo(Path.Combine(project.UnityAssetsDir, Constants.UnityIOSPluginPath, Constants.Unity3DCopyFolderName, package)) 
+
+    copyDirContents (root, lazy (targetDir)) |> ignore
+    if androidSourceDir.Exists then
+        Utils.CleanDir androidTargetDir.FullName 
+        copyDirContents(androidSourceDir, lazy(androidTargetDir)) |> ignore
+    if iOSSourceDir.Exists then
+        Utils.CleanDir iOSTargetDir.FullName 
+        copyDirContents(iOSSourceDir, lazy(iOSTargetDir)) |> ignore
 
 let Install(sources,force, hard, lockFile:LockFile) =
     let extractedPackages = Paket.InstallProcess.createModel(sources,force, lockFile)
@@ -62,40 +80,14 @@ let Install(sources,force, hard, lockFile:LockFile) =
                                        |> Seq.map (fun f -> FileInfo(f)) 
                             let path =Path.Combine(unityProject.UnityAssetsDir, Constants.Unity3DCopyFolderName, installModel.PackageName)
                             path, dlls )
-        |> Seq.iter (fun (path,dlls) -> 
-                                        Utils.CreateDir path
+        |> Seq.iter (fun (path,dlls) -> Utils.CleanDir path
                                         for dll in dlls do dll.CopyTo(Path.Combine(path, dll.Name)) |> ignore )
 
-        verbosefn "usedPackages: %A" usedPackages
+        usedPackages
+        |> findPackagesWithContent
+        |> Seq.iter (fun (package,dir) -> copyContentFiles(unityProject,package,dir) )
 
-        //project.UpdateReferences(model,usedPackages,hard)
         
-        //removeCopiedFiles project
-
-        //let getGitHubFilePath name = 
-        //    (lockFile.SourceFiles |> List.find (fun f -> Path.GetFileName(f.Name) = name)).FilePath
-
-//        let gitHubFileItems =
-//            referenceFile.GitHubFiles
-//            |> List.map (fun file -> 
-//                             { BuildAction = project.DetermineBuildAction file.Name 
-//                               Include = createRelativePath project.FileName (getGitHubFilePath file.Name)
-//                               Link = Some(if file.Link = "." then Path.GetFileName(file.Name)
-//                                           else Path.Combine(file.Link, Path.GetFileName(file.Name))) })
-//        
-        let nuGetFileItems =
-            if lockFile.Options.OmitContent then [] else
-            copyContentFiles(unityProject, findPackagesWithContent usedPackages)
-        
-        ()
-            
-//            files |> List.map (fun file -> 
-//                                    { BuildAction = project.DetermineBuildAction file.Name
-//                                      Include = createRelativePath project.FileName file.FullName
-//                                      Link = None })
-
-//        project.UpdateFileItems(gitHubFileItems @ nuGetFileItems, hard)
-
-//        project.Save() 
-    
+        verbosefn "packagesWithContent: %A" (usedPackages |> findPackagesWithContent)
+           
     ()
