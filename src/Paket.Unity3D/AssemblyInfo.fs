96d2a3009ab79294b6f978c9ba2ba74c7e5c8988
devboy <dominic.graefen@gmail.com>
2014-11-06 10:53:18 +0100

    Initial Scaffold

---
+++ src/Paket.Unity3D/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("Paket.Unity3D")>]
+[<assembly: AssemblyProductAttribute("Paket.Unity3D")>]
+[<assembly: AssemblyDescriptionAttribute("Piggy-backs ontop of Paket to add dependencies to Unity3D projects")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
---
+++ src/Paket.Unity3D/Library.fs
@@ -0,0 +1,16 @@
+namespace Paket.Unity3D
+
+/// Documentation for my library
+///
+/// ## Example
+///
+///     let h = Library.hello 1
+///     printfn "%d" h
+///
+module Library = 
+  
+  /// Returns 42
+  ///
+  /// ## Parameters
+  ///  - `num` - whatever
+  let hello num = 42
