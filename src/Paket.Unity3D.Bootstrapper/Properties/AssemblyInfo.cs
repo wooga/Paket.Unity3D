113d53f35c8d0ee1d21f35a3f544b433fd521715
devboy <dominic.graefen@gmail.com>
2015-04-14 11:50:54 +0200

    Bump version to 0.1.0

--- src/Paket.Unity3D.Bootstrapper/Properties/AssemblyInfo.cs
+++ src/Paket.Unity3D.Bootstrapper/Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("Paket.Unity3D.Bootstrapper")]
 [assembly: AssemblyProductAttribute("Paket.Unity3D")]
 [assembly: AssemblyDescriptionAttribute("Piggy-backs ontop of Paket to add dependencies to Unity3D projects")]
-[assembly: AssemblyVersionAttribute("0.0.14")]
-[assembly: AssemblyFileVersionAttribute("0.0.14")]
+[assembly: AssemblyVersionAttribute("0.1.0")]
+[assembly: AssemblyFileVersionAttribute("0.1.0")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "0.0.14";
+        internal const string Version = "0.1.0";
     }
 }
