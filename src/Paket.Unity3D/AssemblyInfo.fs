97c4a1f72c90dc2a97f6d6ec6c18ba535d968727
devboy <dominic.graefen@gmail.com>
2015-07-24 13:59:41 +0200

    Version 0.2.0

--- src/Paket.Unity3D/AssemblyInfo.fs
+++ src/Paket.Unity3D/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Paket.Unity3D")>]
 [<assembly: AssemblyProductAttribute("Paket.Unity3D")>]
 [<assembly: AssemblyDescriptionAttribute("Piggy-backs ontop of Paket to add dependencies to Unity3D projects")>]
-[<assembly: AssemblyVersionAttribute("0.1.0")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
+[<assembly: AssemblyVersionAttribute("0.2.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.2.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.1.0"
+    let [<Literal>] Version = "0.2.0"
